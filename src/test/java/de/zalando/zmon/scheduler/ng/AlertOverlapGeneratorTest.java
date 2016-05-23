170092c613d2da88e41772f89696ad3cde6e412c
Henning Jacobs <henning@jacobs1.de>
2016-05-23 18:07:54 +0200

    add empty JUnit test

---
+++ src/test/java/de/zalando/zmon/scheduler/ng/AlertOverlapGeneratorTest.java
@@ -0,0 +1,14 @@
+package de.zalando.zmon.scheduler.ng;
+
+import org.junit.Test;
+
+public class AlertOverlapGeneratorTest {
+
+    @Test
+    public void test() {
+
+        AlertOverlapGenerator gen = new AlertOverlapGenerator(null, null, null, null);
+
+    }
+
+}
